93a7d89f01266ecbbc92dff3058ee81f96141a25
Bob Kerns <rwk@acm.org>
2019-10-17 13:49:35 -0700

    Try using relative and absolute paths for test matching.

--- config/top-jest.config.ts
+++ config/top-jest.config.ts
@@ -6,7 +6,7 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
       testMatch: [
-          "*.ts",
+          "src/__tests__/hello-world.ts",
     "**/__tests__/*.{ts,tsx,js,jsx,mjs}",
       "!**/*.d.ts?(x)",
       "!**/suite-*.*",
