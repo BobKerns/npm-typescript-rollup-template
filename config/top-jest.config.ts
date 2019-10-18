6b2353329a78f497f9e4bb8514b076bb1da9f553
Bob Kerns <rwk@acm.org>
2019-10-17 19:40:42 -0700

    Try pointing jest at the tests' tsconfig

--- config/top-jest.config.ts
+++ config/top-jest.config.ts
@@ -3,13 +3,20 @@
  */
 
 export default {
-  preset: 'ts-jest',
+    preset: 'ts-jest',
+    globals: {
+      "ts-jest": {
+          "tsConfig": "src/__tests__/tsconfig.json",
+      }
+    },
     "modulePaths": [
         "<rootDir>/lib",
         "<rootDir>/build/src"
     ],
   testMatch: [
     "**/__tests__/*.{ts,tsx,js,jsx,mjs}",
+      "!**/src/*.{ts,tsx,js,jsx,mjs}",
+      "!**/node_modules/**",
       "!**/lib/**",
       "!**/build/**",
       "!**/*.d.ts?(x)",
