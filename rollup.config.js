13facbf250b1e99efac723e94a57670e76b0d779
Bob Kerns <1154903+BobKerns@users.noreply.github.com>
2023-03-04 17:32:23 -0800

    Slowly modernizing.

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,7 @@
 // Uncomment to debug require-from in context.
 // process.env.DEBUG_REQUIRE_FROM = "true";
 // noinspection JSUnresolvedVariable
-const requireFrom = require("./build/config/require-from").requireFrom;
-
-const requireConfig = requireFrom(module, "./config", "./build/config");
 
 // noinspection JSUnusedGlobalSymbols
-export default requireConfig('./config/rollup.config').default;
+import * as config from './build/config/rollup.config';
+export default config;
